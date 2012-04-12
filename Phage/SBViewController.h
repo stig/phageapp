93baa16277660db22511ea1d24bdbec8af829a79
Stig Brautaset <stig@brautaset.org>
2012-04-12 21:50:11 +0100

    First cut at an interface. Display -description of state in a UITextView (!) and supply a button that performs a move. (Exactly _what_ move is outside user control.) Send game state to GameCenter, and restore this state on other end. This just shows that we are able to enter / leave games in progress, etc now.

--- Phage/SBViewController.h
+++ Phage/SBViewController.h
@@ -6,8 +6,17 @@
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
+@class SBState;
+
 @interface SBViewController : UIViewController <GKTurnBasedMatchmakerViewControllerDelegate>
 
+@property (strong) IBOutlet UIButton *moveButton;
+@property (strong) IBOutlet UITextView *textView;
+
+@property (strong) GKTurnBasedMatch *currentMatch;
+@property (strong) SBState *currentState;
+
 - (IBAction)go;
+- (IBAction)makeMove;
 
 @end
--- Phage/SBViewController.m
+++ Phage/SBViewController.m
@@ -7,6 +7,9 @@
 //
 
 #import "SBViewController.h"
+#import "SBState.h"
+#import "SBMovePicker.h"
+#import "SBMove.h"
 
 @interface SBViewController ()
 
@@ -14,20 +17,22 @@
 
 @implementation SBViewController
 
-- (void)viewDidLoad
-{
+@synthesize currentMatch = _currentMatch;
+@synthesize currentState = _currentState;
+@synthesize textView = _textView;
+@synthesize moveButton = _moveButton;
+
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)viewDidUnload
-{
+- (void)viewDidUnload {
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
 
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
     if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
         return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
     } else {
@@ -41,7 +46,7 @@
     GKMatchRequest *request = [[GKMatchRequest alloc] init];
     request.minPlayers = 2;
     request.maxPlayers = 2;
-    
+
     GKTurnBasedMatchmakerViewController *vc = [[GKTurnBasedMatchmakerViewController alloc] initWithMatchRequest:request];
     vc.turnBasedMatchmakerDelegate = self;
     vc.showExistingMatches = YES;
@@ -53,31 +58,69 @@
 
 // The user has cancelled
 - (void)turnBasedMatchmakerViewControllerWasCancelled:(GKTurnBasedMatchmakerViewController *)viewController {
-    [viewController dismissModalViewControllerAnimated:YES];
     NSLog(@"User cancelled");
+    [viewController dismissModalViewControllerAnimated:YES];
 }
 
 // Matchmaking has failed with an error
 - (void)turnBasedMatchmakerViewController:(GKTurnBasedMatchmakerViewController *)viewController didFailWithError:(NSError *)error {
-    [viewController dismissModalViewControllerAnimated:YES];
     NSLog(@"didFailWithError: %@", error);
+    [viewController dismissModalViewControllerAnimated:YES];
+}
+
+- (void)enterNewGame:(GKTurnBasedMatch *)match1 {
+    NSLog(@"enterNewGame");
+    self.currentState = [[SBState alloc] init];
+    self.textView.text = [self.currentState description];
+    self.moveButton.enabled = YES;
+}
+
+- (void)takeTurn:(GKTurnBasedMatch *)match1 {
+    NSLog(@"takeTurn");
+    self.currentState = [NSKeyedUnarchiver unarchiveObjectWithData:match1.matchData];
+    self.textView.text = [self.currentState description];
+    self.moveButton.enabled = YES;
+}
+
+- (void)layoutMatch:(GKTurnBasedMatch *)match1 {
+    NSLog(@"layoutMatch");
+    self.currentState = [NSKeyedUnarchiver unarchiveObjectWithData:match1.matchData];
+    self.textView.text = [self.currentState description];
+    self.moveButton.enabled = NO;
 }
 
 // A turned-based match has been found, the game should start
 - (void)turnBasedMatchmakerViewController:(GKTurnBasedMatchmakerViewController *)viewController didFindMatch:(GKTurnBasedMatch *)match {
     [viewController dismissModalViewControllerAnimated:YES];
     NSLog(@"A Match has been found!: %@", match);
+
+    self.currentMatch = match;
+
     // TODO: Should launch a game; handle these cases
     // 1a) Set up initial game in turn
     // 1b) Set up game in turn
     // 2) Set up game out of turn (for view only)
-    // 2b) Game Over
+
+    GKTurnBasedParticipant *firstParticipant = [match.participants objectAtIndex:0];
+
+    if (firstParticipant.lastTurnDate == NULL) {
+        // It's a new game!
+        [self enterNewGame:match];
+    } else {
+        if ([match.currentParticipant.playerID isEqualToString:[GKLocalPlayer localPlayer].playerID]) {
+            // It's your turn!
+            [self takeTurn:match];
+        } else {
+            // It's not your turn, just display the game state.
+            [self layoutMatch:match];
+        }
+    }
 }
 
-- (GKTurnBasedParticipant*)nextParticipantForMatch:(GKTurnBasedMatch*)match {
+- (GKTurnBasedParticipant *)nextParticipantForMatch:(GKTurnBasedMatch *)match {
     NSUInteger currIdx = [match.participants indexOfObject:match.currentParticipant];
     NSUInteger nextIdx = (currIdx + 1) % match.participants.count;
-    return [match.participants objectAtIndex: nextIdx];
+    return [match.participants objectAtIndex:nextIdx];
 }
 
 // Called when a users chooses to quit a match and that player has the current turn.  The developer should call playerQuitInTurnWithOutcome:nextPlayer:matchData:completionHandler: on the match passing in appropriate values.  They can also update matchOutcome for other players as appropriate.
@@ -86,15 +129,41 @@
 
     GKTurnBasedParticipant *part = [self nextParticipantForMatch:match];
     part.matchOutcome = GKTurnBasedMatchOutcomeWon;
-    
+
     [match participantQuitInTurnWithOutcome:GKTurnBasedMatchOutcomeLost
                             nextParticipant:part
-                                  matchData:[NSData data] // TODO: should send current state
+                                  matchData:match.matchData
                           completionHandler:^(NSError *error) {
                               if (error)
                                   NSLog(@"ERROR: %@", error);
                           }];
 
+}
+
+- (IBAction)makeMove {
+    SBPlayer player = [self.currentMatch.participants indexOfObject:self.currentMatch.currentParticipant] == 0
+            ? SBPlayerNorth
+            : SBPlayerSouth;
+
+    SBMove *move = [[[SBMovePicker alloc] init] optimalMoveForState:self.currentState withPlayer:player];
+
+    if (move) {
+        SBState *newState = [self.currentState successorWithMove:move];
+
+        [self.currentMatch endTurnWithNextParticipant:[self nextParticipantForMatch:self.currentMatch]
+                                            matchData:[NSKeyedArchiver archivedDataWithRootObject:newState]
+                                    completionHandler:^(NSError *error) {
+                                        if (error) {
+                                            NSLog(@"%@", error);
+                                            // statusLabel.text = @"Oops, there was a problem.  Try that again.";
+                                        } else {
+                                            self.currentState = newState;
+                                            self.textView.text = [newState description];
+                                            self.moveButton.enabled = NO;
+                                        }
+                                    }];
+
+    }
 }
 
 @end
