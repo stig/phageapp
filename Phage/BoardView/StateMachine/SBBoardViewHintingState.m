808a7d300376583c62446c15d65deab4941e8fde
Stig Brautaset <stig@brautaset.org>
2012-05-22 20:13:07 +0100

    Highlight legal destinations in the hinting state
    Still needs some "make pretty" work. But as a proof of concept it is great :)

--- Phage/BoardView/StateMachine/SBBoardViewHintingState.m
+++ Phage/BoardView/StateMachine/SBBoardViewHintingState.m
@@ -7,8 +7,29 @@
 
 #import "SBBoardViewHintingState.h"
 #import "SBBoardViewUnselectedState.h"
+#import "SBBoardView.h"
+#import "SBPieceLayer.h"
+#import "SBLocation.h"
+#import "SBPiece.h"
+#import "SBCellLayer.h"
 
 @implementation SBBoardViewHintingState
+
+- (void)transitionIn {
+    [super transitionIn];
+
+    SBPiece *piece = self.selectedPieceLayer.piece;
+    for (SBCellLayer *cell in self.delegate.cellLayer.sublayers) {
+        cell.highlighted = [self.delegate.delegate canMovePiece:piece toLocation:cell.location];
+    }
+}
+
+- (void)transitionOut {
+    [super transitionOut];
+    for (SBCellLayer *cell in self.delegate.cellLayer.sublayers) {
+        cell.highlighted = NO;
+    }
+}
 
 - (void)touchesEnded:(NSSet *)touches {
     [self touchesEnded:touches nextStateClass:[SBBoardViewUnselectedState class]];
