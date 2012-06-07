9ed594afd819b6a5967d03dc1eb63a7fc8937df3
Stig Brautaset <stig@brautaset.org>
2012-06-08 00:33:41 +0100

    Notify player that they have to wait for their turn in readonly state.

--- Phage/BoardView/SBBoardViewControllerReadonlyState.m
+++ Phage/BoardView/SBBoardViewControllerReadonlyState.m
@@ -8,4 +8,34 @@
 #import "SBBoardViewControllerReadonlyState.h"
 
 @implementation SBBoardViewControllerReadonlyState
+
+
+- (void)displayMessage {
+    [[[UIAlertView alloc] initWithTitle:@"Opponent Turn"
+                                message:@"Please wait for your opponent to make a move."
+                               delegate:nil
+                      cancelButtonTitle:@"OK"
+                      otherButtonTitles:nil] show];
+}
+
+- (void)handleSingleTapWithPiece:(SBPiece *)piece {
+    [super handleSingleTapWithPiece:piece];
+    [self displayMessage];
+}
+
+- (void)handleSingleTapWithLocation:(SBLocation *)location {
+    [super handleSingleTapWithLocation:location];
+    [self displayMessage];
+}
+
+- (void)handleDoubleTapWithPiece:(SBPiece *)piece {
+    [super handleDoubleTapWithPiece:piece];
+    [self displayMessage];
+}
+
+- (void)handleDoubleTapWithLocation:(SBLocation *)location {
+    [super handleDoubleTapWithLocation:location];
+    [self displayMessage];
+}
+
 @end
