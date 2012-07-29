869758fd274fb34b67b6e8aa613e25b193ab30da
Stig Brautaset <stig@brautaset.org>
2012-07-29 23:47:10 +0100

    Extract model code and tests to its own target - for tidiness and hypothetical re-use in a desktop game

---
+++ PhageModel/PhageModel.h
@@ -0,0 +1,20 @@
+//
+//  PhageModel.h
+//  PhageModel
+//
+//  Created by Stig Brautaset on 29/07/2012.
+//
+//
+
+
+#import "SBBoard.h"
+#import "SBCircle.h"
+#import "SBDiamond.h"
+#import "SBDirection.h"
+#import "SBLocation.h"
+#import "SBMatch.h"
+#import "SBMove.h"
+#import SBPiece.h"
+#import "SBPlayer.h"
+#import "SBSquare.h"
+#import "SBTriangle.h"
