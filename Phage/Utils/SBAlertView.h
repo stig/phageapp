4410eb4f25f84a49b0103da016aa6d4ea6037b2b
Stig Brautaset <stig@brautaset.org>
2012-08-07 00:32:33 +0100

    Implement UIAlertView subclass which takes a block so we don't have to implement UIAlertViewDelegate in our using class

---
+++ Phage/Utils/SBAlertView.h
@@ -0,0 +1,11 @@
+//
+// Created by SuperPappi on 05/08/2012.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+
+@interface SBAlertView : UIAlertView
+- (id)initWithTitle:(NSString *)title message:(NSString *)message completion:(void (^)(NSInteger buttonIndex))completion cancelButtonTitle:(NSString *)cancelButtonTitle otherButtonTitles:(NSString *)otherButtonTitles, ... NS_REQUIRES_NIL_TERMINATION;
+@end
---
+++ Phage/Utils/SBAlertView.m
@@ -0,0 +1,28 @@
+//
+// Created by SuperPappi on 05/08/2012.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import "SBAlertView.h"
+
+@interface SBAlertView () < UIAlertViewDelegate >
+@property (copy, nonatomic) void (^completion)(NSInteger);
+@end
+
+@implementation SBAlertView
+@synthesize completion;
+
+- (id)initWithTitle:(NSString *)title message:(NSString *)message completion:(void (^)(NSInteger buttonIndex))theCompletion cancelButtonTitle:(NSString *)cancelButtonTitle otherButtonTitles:(NSString *)otherButtonTitles, ... {
+    self.completion = theCompletion;
+    return [super initWithTitle:title message:message delegate:self
+              cancelButtonTitle:cancelButtonTitle otherButtonTitles:otherButtonTitles, nil];
+}
+
+- (void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex {
+    if (self.completion)
+        self.completion(buttonIndex);
+}
+
+@end
