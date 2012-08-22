2e8d30da871a74c0314b99714d201a3699fa1ab4
Stig Brautaset <stig@brautaset.org>
2012-08-22 23:53:57 +0100

    Detect touches in window and, if piece is selected, calculate their board location

--- Phage/SBBoardView.m
+++ Phage/SBBoardView.m
@@ -12,6 +12,7 @@
 
 @interface SBBoardView () < SBPieceViewDelegate >
 
+@property (nonatomic) CGSize cellSize;
 @property (weak, nonatomic) SBPieceView *selected;
 
 @property (copy, nonatomic) SBBoard *board;
@@ -52,12 +53,12 @@
 }
 
 - (void)setupLocations {
-    CGSize sz = CGSizeMake(self.bounds.size.width / 8.0, self.bounds.size.height / 8.0);
+    self.cellSize = CGSizeMake(self.bounds.size.width / 8.0, self.bounds.size.height / 8.0);
 
     NSMutableDictionary *locations = [NSMutableDictionary dictionary];
 
     [self.board enumerateLocationsUsingBlock:^(SBLocation *loc) {
-        CGPoint p = CGPointMake((0.5 + loc.column) * sz.width, (0.5 + loc.row) * sz.height);
+        CGPoint p = CGPointMake((0.5 + loc.column) * self.cellSize.width, (0.5 + loc.row) * self.cellSize.height);
         [locations setObject:[NSValue valueWithCGPoint:p] forKey:loc];
     }];
 
@@ -90,5 +91,26 @@
     self.selected.highlighted = YES;
 }
 
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+    [super touchesEnded:touches withEvent:event];
+
+    if (!self.selected) {
+        NSLog(@"Touches ended but no piece selected; nothing to do");
+        return;
+    }
+
+    UITouch *touch = [touches anyObject];
+    CGPoint point = [touch locationInView:self];
+    SBLocation *location = [self locationInGridFromPoint:point];
+
+    NSLog(@"location = %@", location);
+
+}
+
+- (SBLocation *)locationInGridFromPoint:(CGPoint)point {
+    NSUInteger col = (NSUInteger)(point.x / self.cellSize.width);
+    NSUInteger row = (NSUInteger)(point.y / self.cellSize.height);
+    return [SBLocation locationWithColumn:col row:row];
+}
 
 @end
